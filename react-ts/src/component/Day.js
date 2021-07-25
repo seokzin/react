0007832b2ff665de9f2a9157eae2e626c9401f94
seokzin <seokzin95@gmail.com>
2021-07-26 02:51:38 +0900

    feat: Make react word voca app
    - React 기본 문법으로 영어단어 앱 제작

---
+++ react-ts/src/component/Day.js
@@ -0,0 +1,25 @@
+import dummy from "../db/data.json";
+import Word from "./Word";
+import { useParams } from "react-router-dom";
+
+const Day = () => {
+  const { day } = useParams();
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
+
+  return (
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map((word) => (
+            <>
+              <Word word={word} key={word.id} />
+            </>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Day;
