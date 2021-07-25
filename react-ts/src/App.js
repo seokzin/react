f0ebfeb3819aebd9818195c677e0ac19da594a76
seokzin <seokzin95@gmail.com>
2021-07-26 02:51:38 +0900

    feat: Make react word voca app
    - React 기본 문법으로 영어단어 앱 제작

---
+++ react-ts/src/App.js
@@ -0,0 +1,28 @@
+import EmptyPage from "./component/EmptyPage";
+import DayList from "./component/DayList";
+import Day from "./component/Day";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route>
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
