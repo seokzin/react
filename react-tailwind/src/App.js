b5c9cf24e12c8d1f87426fd042a9d65339406882
seokzin <seokzin95@gmail.com>
2021-07-28 22:33:40 +0900

    refac: Make css set from same styles
    - 중복되는 스타일을 묶어서 하나의 CSS 요소로 만듦
    - 요소의 재활용을 통해 같은 스타일 적용 가능

--- react-tailwind/src/App.js
+++ react-tailwind/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Content />
+      <Footer />
     </>
   );
 }
--- react-tailwind/src/components/Content.jsx
+++ react-tailwind/src/components/Content.jsx
@@ -5,17 +5,17 @@ import ImageTwo from "../images/egg-2.jpg";
 const Content = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img src={ImageOne} alt="egg" className="h-full rounded mb-20 shadow" />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Egg Muffins</h2>
           <p className="mb-2">Cripsy, delicious, and nutritious</p>
           <span>$16</span>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img src={ImageTwo} alt="egg" className="h-full rounded mb-20 shadow" />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Egg Salsd</h2>
           <p className="mb-2">Cripsy, delicious, and nutritious</p>
           <span>$8</span>
