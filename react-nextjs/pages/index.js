769e528d2a8f68818887e29cc900eefc8658cfb1
seokzin <seokzin95@gmail.com>
2021-09-13 23:23:05 +0900

    feat: SSR with getServerSideProps

--- react-nextjs/pages/index.js
+++ react-nextjs/pages/index.js
@@ -1,10 +1,30 @@
 import HeadInfo from "./../components/HeadInfo";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <HeadInfo />
       <h1>Welcome to My Blog</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_start=0&_end=10"
+  );
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 20,
+  };
+};
