c20f86e157e8302b6f7f639bb3f39bd634b0111a
seokzin <seokzin95@gmail.com>
2021-09-17 17:47:48 +0900

    feat: Install styled-components

--- react-styledcomponent/src/App.js
+++ react-styledcomponent/src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 
 const App = () => {
-  return <div calssName="App"></div>;
+  return (
+    <>
+      <Container>
+        <Button>Hello</Button>
+        <Button danger>Hello</Button>
+      </Container>
+    </>
+  );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: lightblue;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  background-color: ${(props) => (props.danger ? "red" : "blue")};
+`;
 
 export default App;
