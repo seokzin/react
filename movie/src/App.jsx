80a759e7c692fbec6c021d27462023e1e056b769
seokzin <seokzin95@gmail.com>
2021-09-21 22:40:44 +0900

    feat: useEffect on count state

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [count, setCount] = useState(0);
 
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
   };
+
+  const countUp = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log("first render");
+  }, []);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
 
   return (
     <>
@@ -29,6 +42,10 @@ const App = () => {
         />
 
         <button type="submit">Login</button>
+
+        <div>
+          <button onClick={countUp}>{count}</button>
+        </div>
       </form>
     </>
   );
