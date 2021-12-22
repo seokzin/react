d5cd44d3f215958e6a821657a3b6035271027178
seokzin <seokzin95@gmail.com>
2021-12-22 21:35:44 +0900

    feat: Add socketIO server

--- chat/app.js
+++ chat/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const http = require("http");
 const app = express();
 const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "src")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server on: ${PORT}`));
+io.on("connection", (socket) => {
+  console.log("socket connected");
+});
+
+// nodemon app.js -> js 변경마다 서버 새로고침
+server.listen(PORT, () => console.log(`server on: ${PORT}`));
