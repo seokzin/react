8ea19e633d8ee44df0044f6a2839eb6c944d7b5e
seokzin <seokzin95@gmail.com>
2022-01-25 23:32:49 +0900

    feat: add useInput hook

---
+++ react-hooks/src/hooks/useInput/useInput.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { props: { value, onChange }, utils: { setValue } };
+};
